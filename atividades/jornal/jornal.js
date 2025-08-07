c6d326629b4eecca864e3f84bace3193ed673175
Gabriel <gabrielgislaine369@gmail.com>
2025-08-07 17:02:31 -0300

    atividades

---
+++ atividades/jornal/jornal.js
@@ -0,0 +1,15 @@
+function inicio() {
+    window.location.href = "jornal.html";
+}
+
+function politica() {
+    window.location.href = "jornal2.html";
+}
+
+function esportes() {
+    window.location.href = "jornal3.html";
+}
+
+function tecnologia() {
+    window.location.href = "jornal4.html";
+}
